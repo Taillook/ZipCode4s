e02c51cad9092a13932d89fc66df1b5499dbed3e
Taillook <jousoku@gmail.com>
2017-08-07 21:00:17 +0900

    [Add] DEMO project

---
+++ DEMO/DEMO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DEMO
+//
+//  Created by Taillook on 2017/08/07.
+//  Copyright © 2017年 Taillook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
