6f3115810905c7c324f3f02660d08bd09a3e37e3
Taillook <jousoku@gmail.com>
2017-08-07 17:30:04 +0900

    [Add] library project

---
+++ ZipCode4s/ZipCode4s.h
@@ -0,0 +1,19 @@
+//
+//  ZipCode4s.h
+//  ZipCode4s
+//
+//  Created by Taillook on 2017/08/07.
+//  Copyright © 2017年 Taillook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZipCode4s.
+FOUNDATION_EXPORT double ZipCode4sVersionNumber;
+
+//! Project version string for ZipCode4s.
+FOUNDATION_EXPORT const unsigned char ZipCode4sVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZipCode4s/PublicHeader.h>
+
+
